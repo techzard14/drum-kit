261feedcf194f35172301fd8056c69a96ba675ed
techzard14 <uzokwevalentine8@gmail.com>
2022-10-03 19:48:15 +0100

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const kits = ["crash", "kick", "snare", "tom"];
+
+const containerEl = document.querySelector(".container");
+
+kits.forEach((kit) => {
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn")
+    btnEl.innerText = kit;
+    btnEl.style.backgroundImage = "url (images/" + kit + ".png) "
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    audioEl.src = "sounds/" + kit + ".mp3"
+    containerEl.appendChild(audioEl);
+});
